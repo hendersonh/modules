2c9baaad8b82b0759f0cdd32a701ee37170cd90c
Henderson Hood <hendersonhood.com>
2023-04-30 17:13:12 -0400

    templatefile correction

--- services/webserver-cluster/main.tf
+++ services/webserver-cluster/main.tf
@@ -7,9 +7,10 @@ resource "aws_launch_configuration" "example" {
     server_port = var.server_port
     //db_address  = data.terraform_remote_state.db.outputs.address
     //db_port     = data.terraform_remote_state.db.outputs.port
-    db_address = "henderson.hood.com"
-    server_text = var.server_text
-    db_port    = "5858"
+    db_address  = "henderson.hood.com"
+    //server_text = var.server_text
+    server_text = "This is my server: keep out!" 
+    db_port     = "5858"
   })
 
   # Required when using a launch configuration with an auto scaling group.
@@ -175,7 +176,7 @@ resource "aws_autoscaling_schedule" "scale_out_during_business_hours" {
   count                  = var.enable_autoscaling ? 1 : 0
   scheduled_action_name  = "${var.cluster_name}-scale-out-duringbusiness-hours"
   min_size               = 2
-  max_size               = 10 
+  max_size               = 10
   desired_capacity       = 4
   recurrence             = "0 9 * * *"
   autoscaling_group_name = aws_autoscaling_group.example.name
